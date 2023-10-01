411d135d87878ae68c7dce444c22bc99fff21014
Leonardo Freire Russi <leonardofreire.russi@icloud.com>
2023-10-01 17:28:58 -0300

    created 'create user' route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,23 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
 import { knex } from '../database'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const table = await knex('sqlite_schema').select('*')
-    return table
+  app.post('/create', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      email: z.string(),
+      password: z.string(),
+    })
+
+    const { email, password } = createUserBodySchema.parse(request.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      email,
+      hash_password: password,
+    })
+
+    return reply.status(201).send()
   })
 }
