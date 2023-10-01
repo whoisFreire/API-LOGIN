2d3075296c15a5ad6a7ca0db3990930cab616d97
Leonardo Freire Russi <leonardofreire.russi@icloud.com>
2023-10-01 16:47:57 -0300

    create migration 'create-users'

---
+++ db/migrations/20231001193116_create-users.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary().index()
+    table.string('email').notNullable()
+    table.string('hash_password').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
