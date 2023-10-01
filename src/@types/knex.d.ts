d7d3e10a7cca02f23cecb4550db12598d733fab6
Leonardo Freire Russi <leonardofreire.russi@icloud.com>
2023-10-01 17:34:32 -0300

    add @types to type tables and columns

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      email: string
+      hash_password: string
+      created_at: string
+    }
+  }
+}
